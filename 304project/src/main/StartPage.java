0d543923baa7decf5367e6d51ec1714fd6061d6d
Louis Belleville <louis0@telus.net>
2016-06-14 23:04:26 -0700

    add bunny

--- 304project/src/main/StartPage.java
+++ 304project/src/main/StartPage.java
@@ -7,7 +7,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.AbstractButton;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class StartPage extends Page implements ActionListener
@@ -19,12 +21,14 @@ public class StartPage extends Page implements ActionListener
 	{
 		super(mainComponent, dc);
 		startPage = new JPanel();
-		startPage.setLayout(new FlowLayout(FlowLayout.CENTER, 50, 350));
+		startPage.setLayout(null);
+		startPage.setPreferredSize(new Dimension(800,800));
 	}
 
 	@Override
 	public void createPage()
 	{
+
 		
     	JButton b1 = new JButton("Admin Login");
         b1.setVerticalTextPosition(AbstractButton.BOTTOM);
@@ -32,8 +36,9 @@ public class StartPage extends Page implements ActionListener
         b1.setActionCommand("gotoAdminLoginPage");
         b1.addActionListener(mainComponent);
         b1.setPreferredSize(new Dimension(200,70));
+        b1.setBounds(150, 400, 200, 70);
 
-        startPage.add(b1, BorderLayout.WEST);
+        startPage.add(b1);
         
         JButton b2 = new JButton("Customer Login");
         b2.setVerticalTextPosition(AbstractButton.BOTTOM);
@@ -41,10 +46,23 @@ public class StartPage extends Page implements ActionListener
         b2.setActionCommand("gotoCustomerLoginPage");
         b2.addActionListener(mainComponent);
         b2.setPreferredSize(new Dimension(200,70));
+        b2.setBounds(450, 400, 200, 70);
 
-        startPage.add(b2, BorderLayout.EAST);
+        startPage.add(b2);
         
+		JLabel l1 = new JLabel(new ImageIcon("./images/LAAGlogo.png"));
+		l1.setBounds(60,0, 680,289);
+		startPage.add(l1);
+		
+		JLabel l2 = new JLabel(new ImageIcon("./images/LAAGbunny.png"));
+		l2.setBounds(188,160, 414,538);
+		startPage.add(l2);
+		
         mainComponent.add(startPage);
+        
+
+        mainComponent.revalidate();
+        mainComponent.repaint();
 	}
 
 	@Override
